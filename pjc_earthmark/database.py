a94c0fd789dce76b97a9b9a7d82cc941fce53ed3
Dimitris Psaradellis <psaradellis@gmail.com>
2022-01-25 20:59:51 +0200

    for linux

--- pjc_earthmark/database.py
+++ pjc_earthmark/database.py
@@ -1,9 +1,9 @@
-from sqlmodel import SQLModel,create_engine
+from sqlmodel import SQLModel, create_engine
 import os
 
 # Get current file directory, and create sqlite url
 BASE_DIR = os.path.dirname(os.path.realpath("__file__"))
-SQLITE_DB_URL = f"sqlite:///{BASE_DIR}\\earthmark.db"
+SQLITE_DB_URL = f"sqlite:///{BASE_DIR}/earthmark.db"
 
 # Create database if it does not exist, else connect to it
 engine = create_engine(SQLITE_DB_URL, connect_args={"check_same_thread": False}, echo=False)
